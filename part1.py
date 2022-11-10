b6774bc3d7fd469d4efdf52fdc15a2ef1616cdfa
josesoto <josesoto@itop.es>
2022-11-10 14:54:02 +0000

    feat(part1): created processFuncionality function

--- part1.py
+++ part1.py
@@ -2,10 +2,19 @@ from multiprocessing import Process
 from time import sleep
 import os
 
+def processFunctionality():
+    print('Empezando el Proceso (%s)...' % ( os.getpid()))
+    sleep(5)
+    print("Se acaba la ejecucion del proceso " + str(os.getpid()))
+    os._exit(0)
+    
+
+
+
 if __name__ == "__main__":
     userInput =  int(input("Cuantos processos hijos debe de tener ? "))
     print('Proceso padre %s.' % os.getpid())
     for i in range (userInput):
-        p = Process(target="some function", args=('test',))
+        p = Process(target=printProcess, args=('test',))
         p.start()
 
